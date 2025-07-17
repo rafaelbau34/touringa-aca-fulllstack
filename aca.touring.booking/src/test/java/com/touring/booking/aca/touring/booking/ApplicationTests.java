a7728f6f01727211d11fe1adfcb62308983088cb
rafael <rafaelbaugar@gmail.com>
2025-07-16 18:10:56 -0600

    starting project

---
+++ aca.touring.booking/src/test/java/com/touring/booking/aca/touring/booking/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.touring.booking.aca.touring.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ aca.touring.service/src/test/java/com/touring/service/aca/touring/service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.touring.service.aca.touring.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
